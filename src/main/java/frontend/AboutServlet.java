1bb5c140ba9ba8539cfef72137574f8ce3d7dc7b
Abdus Salam <salaam.mbstu@gmail.com>
2021-10-21 12:14:45 +0600

    AboutServlet Added

---
+++ src/main/java/frontend/AboutServlet.java
@@ -0,0 +1,35 @@
+package frontend;
+
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AboutServlet
+ */
+@WebServlet("/AboutServlet")
+public class AboutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AboutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("about.jsp");
+		
+		rd.forward(request, response);
+	}
+
+}
