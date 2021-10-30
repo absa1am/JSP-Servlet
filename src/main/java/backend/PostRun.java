48607719462dad4d6a83682a38bf708b6961ad5e
Abdus Salam <salaam.mbstu@gmail.com>
2021-10-30 20:39:53 +0600

    PostRun class added

---
+++ src/main/java/backend/PostRun.java
@@ -0,0 +1,23 @@
+package backend;
+
+public class PostRun implements PostDAO {
+
+	@Override
+	public boolean save(Post post) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean update(Post post) {
+		
+		return false;
+	}
+
+	@Override
+	public Post get(Integer id) {
+		
+		return null;
+	}
+	
+}
